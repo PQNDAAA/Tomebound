82b28784731237b0e1fd907266f7db070144680e
PQNDAAA <105215953+PQNDAAA@users.noreply.github.com>
2023-10-03 14:49:03 +0200

    Project commit

---
+++ Source/Tomebound/TomeboundGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TomeboundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOMEBOUND_API ATomeboundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
